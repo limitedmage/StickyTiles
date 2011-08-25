92cac177f5a4bbd6adcc6f5c648322a3ad2cb159
Juliana Pena <limited.mage@gmail.com>
2011-08-25 14:07:14 -0500

    Now with customizable text and colors!

--- StickyTiles/MainPage.xaml.cs
+++ StickyTiles/MainPage.xaml.cs
@@ -17,10 +17,8 @@ using System.ComponentModel;
 
 namespace StickyTiles {
     public partial class MainPage : PhoneApplicationPage, INotifyPropertyChanged {
-        // Constructor
-        public MainPage() {
-            InitializeComponent();
-        }
+        
+        #region Properties
 
         private string _frontText;
         public string FrontText {
@@ -44,32 +42,86 @@ namespace StickyTiles {
             }
         }
 
+        private Color _frontColor;
+        public Color FrontColor {
+            get {
+                return _frontColor;
+            }
+            set {
+                _frontColor = value;
+                Changed("FrontColor");
+            }
+        }
+
+        private Color _backColor;
+        public Color BackColor {
+            get {
+                return _backColor;
+            }
+            set {
+                _backColor = value;
+                Changed("BackColor");
+            }
+        }
+
+        #endregion
+
+        // Constructor
+        public MainPage() {
+            InitializeComponent();
+
+            DataContext = this;
+
+            FrontColor = (Color) App.Current.Resources["PhoneAccentColor"];
+            BackColor = (Color)App.Current.Resources["PhoneAccentColor"];
+        }
+
         private void PinButton_Click(object sender, RoutedEventArgs e) {
-            
             long id = DateTime.Now.Ticks;
 
             WriteableBitmap front = new WriteableBitmap(TilePreview, null);
             string frontFilename = "Shared/ShellContent/" + id + "-front.jpg";
 
-            WriteableBitmap back = new WriteableBitmap(BackTilePreview, null);
-            string backFilename = "Shared/ShellContent/" + id + "-back.jpg";
-
             using (IsolatedStorageFile isf = IsolatedStorageFile.GetUserStoreForApplication()) {
                 using (IsolatedStorageFileStream fs = isf.CreateFile(frontFilename)) {
                     front.SaveJpeg(fs, front.PixelWidth, front.PixelHeight, 0, 100);
                 }
-                
-                using (IsolatedStorageFileStream fs = isf.CreateFile(backFilename)) {
-                    back.SaveJpeg(fs, back.PixelWidth, back.PixelHeight, 0, 100);
-                }
             }
 
             StandardTileData tile = new StandardTileData {
-                BackgroundImage = new Uri("isostore:/" + frontFilename),
-                BackBackgroundImage = new Uri("isostore:/" + backFilename)
+                BackgroundImage = new Uri("isostore:/" + frontFilename)
             };
 
+            if (EnableBack.IsChecked.GetValueOrDefault()) {
+                WriteableBitmap back = new WriteableBitmap(BackTilePreview, null);
+                string backFilename = "Shared/ShellContent/" + id + "-back.jpg";
+
+                using (IsolatedStorageFile isf = IsolatedStorageFile.GetUserStoreForApplication()) {
+                    using (IsolatedStorageFileStream fs = isf.CreateFile(backFilename)) {
+                        back.SaveJpeg(fs, back.PixelWidth, back.PixelHeight, 0, 100);
+                    }
+                }
+
+                tile.BackBackgroundImage = new Uri("isostore:/" + backFilename);
+            }
+
             ShellTile.Create(new Uri("/MainPage.xaml?tile=" + id, UriKind.Relative), tile);
+        }
+
+        private void ToggleFrontColorPicker(object sender, RoutedEventArgs e) {
+            if (FrontColorPickerOverlay.Visibility == System.Windows.Visibility.Visible) {
+                FrontColorPickerOverlay.Hide();
+            } else {
+                FrontColorPickerOverlay.Show();
+            }
+        }
+
+        private void ToggleBackColorPicker(object sender, RoutedEventArgs e) {
+            if (BackColorPickerOverlay.Visibility == System.Windows.Visibility.Visible) {
+                BackColorPickerOverlay.Hide();
+            } else {
+                BackColorPickerOverlay.Show();
+            }
         }
 
         #region INotifyPropertyChanged Members
